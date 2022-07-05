9a459f704c6d09cabc02be746e9efdb91d695676
JHNovoa90 <jhnovoa1690@gmail.com>
2022-07-05 22:32:29 +0200

    Entrar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,4 +1,4 @@
-//import { Navbar, Container, Nav} from "react-bootstrap/Nabvar";
+
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
