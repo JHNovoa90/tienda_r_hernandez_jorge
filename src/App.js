caa9b54830406f37e2a4d1df54854921c7251171
JHNovoa90 <jhnovoa1690@gmail.com>
2022-07-07 12:16:54 +0200

    entrar

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import NavBar from './components/NavBar/NavBar';
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ItemListContainer from './Containers/ItemListContainer/ItemListContainer';
 
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <ItemListContainer greeting="Hola soy ItemListContainer"/>
     </div>
   );
 }
