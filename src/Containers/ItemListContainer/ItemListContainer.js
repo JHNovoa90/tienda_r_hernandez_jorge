f7cb779067ebd6710d0323d464e2e15a597a9474
JHNovoa90 <jhnovoa1690@gmail.com>
2022-07-12 15:13:26 +0200

    entrar

--- src/Containers/ItemListContainer/ItemListContainer.js
+++ src/Containers/ItemListContainer/ItemListContainer.js
@@ -1,8 +1,16 @@
 import React from "react"
+import ItemCount from "../../components/itemCount"
 
 const ItemListContainer = ({ greeting }) => {
+
+        const onAdd =(count)=>{
+            //console.log(count)
+            alert("Compraste:" + count)
+        }
     return (
-        <div>{ greeting }</div>
+        <div>{ greeting }
+            <ItemCount initial={1} stock={10} onAdd={onAdd}/>
+        </div>
     )
 }
 export default ItemListContainer
