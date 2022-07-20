4d3e4e4da51dccc777a74a88ab2273b6e5559b13
JHNovoa90 <jhnovoa1690@gmail.com>
2022-07-20 02:29:07 +0200

    Nueva entrega

--- src/containers/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/containers/ItemDetailContainer/ItemDetailContainer.jsx
@@ -27,7 +27,6 @@ const ItemDetailContainer = () => {
   }, []) 
   return (
     <div>
-      <p className = 'white-text'> Item Detail Container</p>
       {product === 0 ? 
       <div> 
         <p className = 'white-text'> Loading...</p>
