e257bdf0a7f33cc2e0cced1fe04f826729ad38d2
JHNovoa90 <jhnovoa1690@gmail.com>
2022-07-29 17:33:23 +0200

    reentrega/CartContext

--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -2,13 +2,10 @@ import { useContext } from "react";
 import { useState } from "react";
 import { createContext } from "react";
 
-// Notar que el carrito de compras va a ser un array con objetos de tipo producto adentro
-// cada objeto además va a tener una variable que representa la cantidad de items de ese tipo
-// que han sido comprados
 export const CartContext = createContext([]);
 
 const CartContextProvider = ({children}) => {
-    //Definir los estados y las funciones
+    
     const [cartList, setCartList] = useState([]);
     
     const añadirCarrito = (product) => {
