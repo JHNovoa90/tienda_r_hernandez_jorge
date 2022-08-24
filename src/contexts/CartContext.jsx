86802d648e31805d9fb96a38b416d4f04de1fe93
JHNovoa90 <jhnovoa1690@gmail.com>
2022-08-24 16:50:22 +0200

    Entrega final

--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -11,11 +11,9 @@ const CartContextProvider = ({children}) => {
     const calculateTotalItems = () => {
         let counter = 0;
         cartList.forEach(element => {
-        console.log("Element" , element);
         counter += element.itemQuantity;
         });
         setItemCount(counter);
-        console.log("Counter : ", counter);
       }
     
     const addToCart = (product) => {
@@ -23,32 +21,21 @@ const CartContextProvider = ({children}) => {
         cartList.forEach( (prod) => {
             if (prod.id ===product.id) {
                 productoExistente = true;
-                console.log("El producto ya está añadido al array");
             }
         });
 
         if (productoExistente) {
             let existingProduct = cartList.find((prod) => prod.id === product.id);
-            console.log("El producto recibido por parámetro");
-            console.log(product);
-            console.log("El producto encontrado en el array");
-            console.log(existingProduct);
-            console.log("La cantidad actual", existingProduct.itemQuantity);
-            console.log("La cantidad recibida", product.itemQuantity);
             existingProduct.itemQuantity = existingProduct.itemQuantity + product.itemQuantity;            
-            console.log("La cantidad actualizada : ", existingProduct.itemQuantity);
             calculateTotalItems();
         } else {
             if (firstProduct) {
                 setItemCount(product.itemQuantity);
                 setCartList([...cartList, product]);
                 setFirstProduct(false);
-                console.log("New product, empty array");
-                console.log(product);
             } else {
                 setCartList([...cartList, product]);
                 setItemCount(itemCount + product.itemQuantity);
-                console.log("New product, full array");
             }
         }
     }
