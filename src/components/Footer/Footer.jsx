02e4c44c3c76695d1cb4260c03baea361da98954
JHNovoa90 <jhnovoa1690@gmail.com>
2022-09-02 10:17:41 +0200

    Arreglo

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css'
 const Footer = () => {
   return (
     <div className = 'div-footer'>
-        <p className = 'contact'> Contacto</p>
+        <p className = 'contact'> Contactos</p>
         <p className = 'contact'> Redes Sociales</p>
     </div>
   )
